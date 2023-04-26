0d0ad4181222f2357a08f52a9906d37d2476f0e5
SahsiSunny <sahsisunny@gmail.com>
2023-04-26 17:14:23 +0530

    first commit

---
+++ app/src/main/java/com/sahsisunny/gittrackr/PullRequestsActivity.kt
@@ -0,0 +1,11 @@
+package com.sahsisunny.gittrackr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PullRequestsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pull_requests)
+    }
+}
---
+++ app/src/main/java/com/sahsisunny/gittrackr/SplashScreen.kt
@@ -0,0 +1,33 @@
+package com.sahsisunny.gittrackr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowInsets
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.sahsisunny.gittrackr.databinding.ActivitySplashScreenBinding
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN
+//        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
